d3a800412c2747b4f15681f4951e53198db7a8f6
David Sliren <bluee.97.berry@gmail.com>
2026-01-05 16:06:14 -0500

    feact: Importacion de imagen

--- src/components/Sections/Information.jsx
+++ src/components/Sections/Information.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ImgBackGround from "../ImgBackGround";
+import Merlina from "/src/assets/img/merlina.webp";
 
 function Information() {
-  return <section className="w-full min-h-dvh">Information</section>;
+  return (
+    <section className="relative z-0 w-full min-h-[50dvh] bg-green-300">
+      <ImgBackGround image={Merlina} />
+    </section>
+  );
 }
 
 export default Information;
