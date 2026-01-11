0b155267b72986e01e8a6f6c6a95c7437bc1e3b4
David Sliren <bluee.97.berry@gmail.com>
2026-01-10 22:54:18 -0500

    feact(btnPaginacion)/style(page/Catalogo): implementar botones de paginacion y nuevo diseño de la interfaz de la pagina catalogos

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -3,6 +3,7 @@ import Card from "../components/Card";
 import { useMovie } from "../hooks/useTankStack";
 import { useState } from "react";
 import { useParams } from "react-router";
+import Btn from "../components/Btn";
 
 function Catalogo() {
   const [newpage, setNewPage] = useState(1);
@@ -12,15 +13,22 @@ function Catalogo() {
     setNewPage((prev) => prev + 1);
   }
 
+  function prevPage() {
+    if (newpage === 1) {
+      return;
+    }
+    setNewPage((prev) => prev - 1);
+  }
+
   console.log(catalogo.data);
 
   if (catalogo.isLoading) {
     return <div>Cargando</div>;
   }
   return (
-    <main className="min-h-dvh">
+    <main className=" bg-linear-to-r from-sky-500 min-h-dvh py-2 ">
       {/* <header>Hola</header> */}
-      <section className="bg-zinc-800 grid grid-cols-2 w-full h-full justify-center gap-4 p-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6">
+      <section className="grid grid-cols-2 w-full h-full justify-center gap-4 p-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6">
         {catalogo.data.results?.map((item) => {
           return (
             <Card
@@ -30,6 +38,13 @@ function Catalogo() {
             />
           );
         })}
+      </section>
+      <section className="flex justify-evenly items-center w-full py-2">
+        <Btn text="Prev" onActive={prevPage} isLeft={true} />
+        <span className="bg-black flex justify-center items-center size-10 text-lg font-inter font-semibold text-white rounded-full transition-discrete duration-500 hover:bg-white hover:text-zinc-900 ">
+          {newpage}
+        </span>
+        <Btn text="Next" onActive={nextPage} />
       </section>
     </main>
   );
