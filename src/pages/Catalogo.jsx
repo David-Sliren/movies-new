b7e8cb8648278bf62fa63023f3383c8a6445f36b
David Sliren <bluee.97.berry@gmail.com>
2026-01-11 20:28:26 -0500

    style(Catalogo): reducir cantidad de colomnas en las pantallas xl y 2xl

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -34,7 +34,7 @@ function Catalogo() {
         </span>
       </header>
 
-      <section className="grid grid-cols-2 w-full h-full justify-center gap-4 p-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6">
+      <section className="grid grid-cols-2 w-full h-full justify-center gap-4 p-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
         {catalogo.isError && <ErrorControl text={catalogo.error.message} />}
 
         {catalogo.isLoading && <ErrorControl text="Cargando..." />}
